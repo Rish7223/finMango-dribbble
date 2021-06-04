d8ea361650f4061a0ecc6ff107d3b168a2780c10
Rish7223 <rishabh.7223@gmail.com>
2021-06-04 20:58:50 +0530

    intial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-[#995544] text-2xl font-bold">
+        just checking is tailwind configured or not
+      </h1>
+    </div>
+  );
+}
+
+export default App;
